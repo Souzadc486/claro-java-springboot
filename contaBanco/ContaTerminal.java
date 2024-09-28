f7554e5d59e3acb36e72105e9d8164ba98b2e2c2
souzadc <souzadc486@gmail.com>
2024-09-28 13:11:59 +0000

    feat: terminado o dialogo v1.2

--- contaBanco/ContaTerminal.java
+++ contaBanco/ContaTerminal.java
@@ -1,28 +1,34 @@
-import javal.util.Locale;
+import java.util.Locale;
 import java.util.Scanner;
 
-public class ContaTermial {
+public class ContaTerminal {
  public static void main(String[] args) {
    Scanner scanner= new Scanner(System.in).useLocale(Locale.US);
    
    System.out.println("Bem-vindo a conta banco");
    
-   System.out.println("Por favor digite o Número da conta: ");
+   System.out.print("Digite o Número da conta: ");
    int numeroConta = scanner.nextInt();
 
-   System.out.println("Por favor digite o número da agência: ");
+   System.out.print("Digite o número da agência: ");
    String numeroAgencia = scanner.next();
 
-   System.out.println("Por favor digite o nome do cliente: ");
+   System.out.print("Digite o nome do cliente: ");
    String nomeCliente = scanner.next();
 
-   System.out.println("Por favor Digite o Número da agência: ");
-   double saldo = scanner.nextDouble();
+   System.out.print("Digite o saldo: ");
+   double numeroSaldo = scanner.nextDouble();
 
-   System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + " conta " + numeroConta + " e seu saldo " + saldo + " já está disponivel
-                      para saque");
+  
+  //
+
+  String conta = String.valueOf(numeroConta);
+  String saldo = String.valueOf(numeroSaldo);
+
+  String saudacao = "Olá ".concat(nomeCliente).concat(" obrigado por criar uma conta em nosso banco, ");
+  String dados = "sua agência é " + numeroAgencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.";
+
+  System.out.println(saudacao + dados);
    
  } 
 }
-
-//Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque".
